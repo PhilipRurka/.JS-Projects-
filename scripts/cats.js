fa1f26ebbf93e7e76d6f511c8422316fffdcf137
PhilipRurka <DefFreeman@Philips-MacBook-Pro.local>
2016-05-05 09:58:38 -0400

    Update

--- scripts/cats.js
+++ scripts/cats.js
@@ -1,97 +1,97 @@
-/********************************
-       	    Cats Var
-********************************/
-/*   3     #    */
+// /********************************
+//        	    Cats Var
+// ********************************/
+// /*   3     #    */
 
-var $main_button = $('<button id="main_button">GO!</button>');
-var $select = $('<select id="cats_select"></select>');
-var $option_check = $('<option value="check">Check</option>');
-var $option_image = $('<option value="image">Image</option>');
-var $option_direction = $('<option value="direction">+ -</option>');
+// var $main_button = $('<button id="main_button">GO!</button>');
+// var $select = $('<select id="cats_select"></select>');
+// var $option_check = $('<option value="check">Check</option>');
+// var $option_image = $('<option value="image">Image</option>');
+// var $option_direction = $('<option value="direction">+ -</option>');
 
-//These 'var' are for the content of each type of page lay out.
-var shopping_list = {
-	clothing : [],
-	hygiene : [],
-	electronics : []
-}
-var zero_count = 0;
-var count = 0;
-// var 
+// //These 'var' are for the content of each type of page lay out.
+// var shopping_list = {
+// 	clothing : [],
+// 	hygiene : [],
+// 	electronics : []
+// }
+// var zero_count = 0;
+// var count = 0;
+// // var 
 
-// var
-// var
-// var 
-// var 
+// // var
+// // var
+// // var 
+// // var 
 
-// var
-// var
-// var 
-// var 
-/********************************
-       	   Functions
-********************************/
-/*   3     #    */
+// // var
+// // var
+// // var 
+// // var 
+// /********************************
+//        	   Functions
+// ********************************/
+// /*   3     #    */
 
-//Show the appropreate lay out.
+// //Show the appropreate lay out.
 
-$('.add').each(function () {
-	$(this).click (function (event) {
-		event.preventDefault();
-		count ++;
-		$(this).next().val(count);
+// $('.add').each(function () {
+// 	$(this).click (function (event) {
+// 		event.preventDefault();
+// 		count ++;
+// 		$(this).next().val(count);
 
-		console.log('Add one!')
-	});
-});
+// 		console.log('Add one!')
+// 	});
+// });
 
-$('.remove').each(function () {
-	$(this).click (function (event) {
-		event.preventDefault();
-		previousSibling.
+// $('.remove').each(function () {
+// 	$(this).click (function (event) {
+// 		event.preventDefault();
+// 		previousSibling.
 
-		console.log('Remove one!')
-	});
-});
+// 		console.log('Remove one!')
+// 	});
+// });
 
 
-function check_func () {
-	console.log('Check me out!');
-}
+// function check_func () {
+// 	console.log('Check me out!');
+// }
 
-function image_func () {
-	console.log('Images Yo!');
-}
+// function image_func () {
+// 	console.log('Images Yo!');
+// }
 
-function direction_func () {
-	console.log('Directions?');
-}
+// function direction_func () {
+// 	console.log('Directions?');
+// }
 
-/********************************
-       	     Append
-********************************/
-/*   3     #    */
+// /********************************
+//        	     Append
+// ********************************/
+// /*   3     #    */
 
-//Append each 'var' appropreately then '.hide()'.
+// //Append each 'var' appropreately then '.hide()'.
 
-$select.append($option_check).append($option_image).append($option_direction);
-$('#select_div').append($select).append($main_button);
+// $select.append($option_check).append($option_image).append($option_direction);
+// $('#select_div').append($select).append($main_button);
 
-/********************************
-       Events + Conditions
-********************************/
-/*   3     #    */
+// /********************************
+//        Events + Conditions
+// ********************************/
+// /*   3     #    */
 
-$main_button.click (function (event) {
-	event.preventDefault();
-	if ($select.val() === 'check') {
-		check_func();
-	} else if ($select.val() === 'image') {
-		image_func();
-	}	else {
-		direction_func();
-	}
-});
+// $main_button.click (function (event) {
+// 	event.preventDefault();
+// 	if ($select.val() === 'check') {
+// 		check_func();
+// 	} else if ($select.val() === 'image') {
+// 		image_func();
+// 	}	else {
+// 		direction_func();
+// 	}
+// });
 
 
 
